dfc86451f2b0c884a192d908b2b59a19713c44de
Nitish Kumar <127375721+NitishRao636@users.noreply.github.com>
2026-07-23 12:40:35 +0530

    Delete lib directory

--- lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) { return twMerge(clsx(inputs)); }
-
-export function formatCurrency(value: number | string, currency = "INR", locale = "en-IN") {
-  return new Intl.NumberFormat(locale, { style: "currency", currency, maximumFractionDigits: 0 }).format(Number(value));
-}
